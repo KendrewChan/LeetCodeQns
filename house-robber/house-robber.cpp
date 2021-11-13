5b015d1e86b4db0c2d56eaa2cfcb16f5184e0ccb
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2021-11-13 15:59:59 +0800

    Time: 0 ms (100.00%), Space: 7.6 MB (94.14%) - LeetHub

---
+++ house-robber/house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+        nums[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++) {
+            nums[i] = max(nums[i-1], nums[i] + nums[i-2]);
+        }
+        return nums[nums.size()-1];
+    }
+};
+/*
+[1]
+[2,1]
+[1,2,3,1]
+[2,1,1,2]
+[2,7,9,3,1]
+*/
