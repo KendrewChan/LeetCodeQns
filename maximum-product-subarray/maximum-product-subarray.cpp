7ec2bacc76fdf2d60de03fa1b3b014f54df38308
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2021-11-16 19:18:37 +0800

    Time: 8 ms (38.46%), Space: 11.7 MB (89.71%) - LeetHub

---
+++ maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxPdt = -99;
+        int currMax = 1;
+        int currMin = 1;
+        for (int num : nums) {
+            if (num < 0) {
+                int temp = currMax;
+                currMax = currMin;
+                currMin = temp;
+            }
+            currMax = max(num, num * currMax);
+            currMin = min(num, num * currMin);
+            
+            maxPdt = max(maxPdt, currMax);
+        }
+        return maxPdt;
+    }
+};
+/*
+[2,3,-2,4]
+[-2,0,-1]
+[-2,3,-4]
+*/
