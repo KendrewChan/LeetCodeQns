6af88ff2f5c71d9d3179dc6f6299285499629928
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-01-05 16:18:12 +0800

    Time: 5 ms (5.15%), Space: 6.1 MB (9.55%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -3,6 +3,7 @@ class Solution {
     
 public:
     int numTrees(int n) {
+        // https://leetcode.com/problems/unique-binary-search-trees/discuss/1565543/C%2B%2BPython-5-Easy-Solutions-w-Explanation-or-Optimization-from-Brute-Force-to-DP-to-Catalan-O(N)
         if (dp.count(n)) return dp[n];
         int res = 0;
         for (int i = 1; i <= n; i++) {
