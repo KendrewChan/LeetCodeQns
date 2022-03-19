6308a4f1ca9072dbb17f9200ab36f92749ec87a9
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-03-19 11:55:19 +0800

    Time: 12 ms (98.59%), Space: 13.2 MB (89.34%) - LeetHub

---
+++ 198-house-robber/198-house-robber.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if (len(nums) == 1):
+            return nums[0]
+        
+        for i in range(2,len(nums)):
+            nums[i] = max(nums[i-1], max(nums[i], nums[i]+nums[i-2]))
+            nums[i-1] = max(nums[i-1], nums[i-2])
+        return max(nums[-1], nums[-2])
