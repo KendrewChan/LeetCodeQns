2c793993de5383bce4d81cb4216f794bfb2ec23a
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-04-19 21:36:02 +0800

    Time: 3511 ms (5.04%), Space: 59.1 MB (90.24%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
-        intervals.sort(key=lambda x: x[1])
-        intervals.sort(key=lambda x: x[0])
+        intervals.sort(key=lambda x: x[1]) # Sort by when it ends
+        intervals.sort(key=lambda x: x[0]) # Sort by when it starts (this has greater priority thus sorted last)
         count_stay = 0
-        curr_end = 5*(10**4)
+        curr_end = 5*(10**4) # Maximum
+        print(intervals)
         for i in range(len(intervals)-1, -1, -1):
-            start = intervals[i][0]
-            end = intervals[i][1]
-            if start <= curr_end and end <= curr_end:
-                curr_end = start
+            start, end = intervals[i]
+            if end <= curr_end: # Check that it's within range, if not skip
+                curr_end = start # We automatically choose the interval that starts the latest because of how we sorted them
                 count_stay += 1
         return len(intervals) - count_stay
             
