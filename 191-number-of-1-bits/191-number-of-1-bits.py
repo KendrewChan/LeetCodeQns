1220e2cd95edfe9125522064cb0d127995ef6061
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-05-07 10:53:45 +0800

    Time: 28 ms (42.19%), Space: 13.2 MB (86.55%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -4,11 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # Since length of n is always 32, we can compare it against a mask
         bitcount = 0
-        mask = 1 # Equivalent to 00000000000000000000000000000001
-        for i in range(32):
-            if (n & mask): # Compare rightmost bit
-                bitcount += 1
-            mask = mask << 1 # Shift leftwards 1, e.g. mask becomes 00000000000000000000000000000010
+        while n:
+            n = n&(n-1) # AND operator on n and (n-1) flips the least significant 1-bit of n to 0
+            bitcount += 1
         return bitcount
