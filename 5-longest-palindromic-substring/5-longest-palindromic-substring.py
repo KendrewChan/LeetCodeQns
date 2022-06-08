6c64d75eecfcdf398a1748f577a1f0dba2318fc7
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-06-08 09:55:09 +0800

    Time: 1466 ms (46.73%), Space: 13.8 MB (91.04%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,26 +1,23 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        longest = s[-1]
-        n = len(s)
-        dp = [[0]*n for _ in range(n)] # 2d array, dp[i][j] is True if s[i:j] is a palindrome or not
-        #filling out the diagonal by 1
-        for i in range(n):
-            dp[i][i] = True # Make rightwards diagonal column True
-            longest = s[i] # Idk why this matters and why i can't just set longest = s[-1]
-        
-        for i in range(n-1,-1,-1): # Start from the back
-            for j in range(i+1,n): # Go rightwards
-                if s[i] == s[j] and (j-i==1 or dp[i+1][j-1]):
-                    # Check if letters match. First loop is always correct. 
-                    # Check the leftward diagonal dp[i+1][j=1] see if its correct -> This continues to cascade leftwards depending on length of string. This is the key part of how the DP array knows whether a string is a palindrome or not
-                    dp[i][j] = True
-                    if len(longest) < len(s[i:j+1]):
-                        longest = s[i:j+1]
-                
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) == 1:
+            return s
+        longest = ""
+        for i in range(1, len(s)):
+            even = self.isPalindrome(s, i-1,i)
+            odd = self.isPalindrome(s, i, i)
+            l = self.getLongest(even, odd)
+            longest = self.getLongest(longest, l)
         return longest
-                    
         
+    def getLongest(self, curr: str, toCompare: str) -> str:
+        if len(curr) > len(toCompare):
+            return curr
+        return toCompare
+        
+    def isPalindrome(self, s: str, left: int, right: int) -> str:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left+1:right]
+            
