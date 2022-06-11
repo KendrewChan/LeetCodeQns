8f1fff0d70bcb04da4fe849df3fe6a745c6f8db6
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-06-11 23:14:16 +0800

    Time: 92 ms (13.47%), Space: 18 MB (41.75%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -7,8 +7,17 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         # inorder traversal of BST is an array sorted in the ascending order.
-        def inorder(root):
-            if not root:
-                return []
-            return inorder(root.left) + [root.val] + inorder(root.right)
-        return inorder(root)[k-1]
+        # instead of doing recursively, we can do iteratively
+        stk = []
+        while True:
+            while root != None:
+                stk.append(root) # if current node is left node, this will immediately get popped later on
+                root = root.left # go all the way left, and append everything along the way
+            # At this pt, root == None
+            root = stk.pop() # Reassign with leftmost in current tree
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
+                # Case 1: root.right == None, in that case, we'll just continue to pop from stk
+                # Case 2: root.right != None, int that case we'll loop and append the left elements again
