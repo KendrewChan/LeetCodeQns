1b701cca447b9e5d8ee52e232570c839b9b355f6
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-06-12 19:00:01 +0800

    Time: 76 ms (5.43%), Space: 13.8 MB (97.21%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,9 +1,9 @@
-class Solution(object):
-    def rob(self, nums):
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
+        # at each idx, u can either not rob or rob
         nums[1] = max(nums[0], nums[1])
-
         for i in range(2, len(nums)):
-            nums[i] = max(nums[i] + nums[i - 2], nums[i - 1])
-        return nums[-1]
+            nums[i] = max(nums[i-1], nums[i]+nums[i-2])
+        return max(nums[-1], nums[-2])
