61bd370471ed5fd28a5af50b9be0b54ccc0bf65c
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-06-12 18:37:40 +0800

    Time: 2147 ms (11.03%), Space: 24 MB (96.69%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,12 +1,10 @@
-from queue import PriorityQueue
-
-
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0 for _ in range(len(temperatures))]
         unvisited = [0]
         for i in range(1,len(temperatures)):
             t = temperatures[i]
+            # Key thing to note is that we're looping backwards on the stack
             while len(unvisited) > 0 and t > temperatures[unvisited[-1]]:
                 nxt_idx = unvisited.pop()
                 res[nxt_idx] = i-nxt_idx
