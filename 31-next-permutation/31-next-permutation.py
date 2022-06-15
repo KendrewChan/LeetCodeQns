41f9466fd965e9bcf7e78ef5986f4cb153fd891f
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-06-15 22:34:04 +0800

    Time: 38 ms (97.65%), Space: 13.9 MB (23.39%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        i = len(nums)-2
+        # Find the turning point
+        while i >= 0 and nums[i+1] <= nums[i]:
+            i -= 1; 
+            
+        if i >= 0:
+            j = len(nums)-1
+            # Find the smallest element largest than i
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        # Reverse it
+        nums[i+1:] = nums[i+1:][::-1]
+            
+            
+        
