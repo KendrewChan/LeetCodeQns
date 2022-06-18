82c330a5ee6b7eca77baf5419f7a99858680b856
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-06-18 18:47:57 +0800

    Time: 111 ms (55.73%), Space: 17.3 MB (54.19%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,23 +1,18 @@
-class Solution(object):
-    def insert(self, intervals, newInterval):
-        temp = []
-        seg = newInterval
-        for curr in intervals:
-            if seg[1] < curr[0]: # Append the merged combinations
-                temp.append(seg)
-                seg = curr # This forever leads us back into the 1st condition, since the intervals were sorted in the first place
-            elif seg[0] > curr[1]: # Not within range
-                temp.append(curr) 
-            else: # Merge
-                seg[0] = min(seg[0], curr[0])
-                seg[1] = max(seg[1], curr[1])
-        temp.append(seg)
-        return temp
-# []
-# [5,7]
-# [[1,3],[6,9]]
-# [2,5]
-# [[1,2],[3,5],[6,7],[8,10],[12,16]]
-# [4,8]
-# [[1,5]]
-# [2,3]
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        curr = newInterval
+        for interval in intervals:
+            # Not within range (before)
+            if interval[1] < curr[0]:
+                res.append(interval)
+            # Out of newInterval range (after) -> append
+            elif interval[0] > curr[1]:
+                res.append(curr)
+                curr = interval
+            # Within newInterval range -> merge
+            else:
+                curr[0] = min(curr[0], interval[0])
+                curr[1] = max(curr[1], interval[1])
+        res.append(curr)
+        return res
