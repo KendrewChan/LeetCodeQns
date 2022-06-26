3c5eb3f72fb57e351b77252f76347f4aa99fda0a
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-06-26 21:52:29 +0800

    Time: 14264 ms (6.61%), Space: 14.8 MB (97.54%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,20 +1,11 @@
-class Solution(object):
-    def jump(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        n = len(nums)
-        dp = [-1 for _ in range(n)]
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        dp = [math.inf for _ in range(len(nums))]
         dp[0] = 0
-        for i in range(n):
-            if dp[i] < 0:
-                continue
-            toLoop = i+nums[i]+1
-            endLoop = n if toLoop > n else toLoop
-            for j in range(i+1, endLoop):
-                if dp[j] == -1:
-                    dp[j] = dp[i]+1
-                    continue
+        for i in range(len(nums)):
+            reachable = nums[i]+i
+            for j in range(i+1, reachable+1):
+                if j >= len(nums):
+                    break
                 dp[j] = min(dp[j], dp[i]+1)
         return dp[-1]
