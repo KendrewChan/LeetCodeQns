d41565147857d5044fc95defe671b0da026991c6
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-07-03 16:44:41 +0800

    Time: 244 ms (89.47%), Space: 14 MB (73.68%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
@@ -0,0 +1,33 @@
+class Solution:
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+        mod = pow(10,9)+7
+        dp = [0]*n
+        dp[0] = 1
+        curr = 0
+        for i in range(1,n):
+            prevDelay, prevForget = i-delay, i-forget
+            # print(i, prevDelay, prevForget)
+            dp[i]=dp[i-1]
+            if prevForget >= 0:
+                toRemove = dp[prevForget]
+                for j in range(prevForget,i+1):
+                    if not dp[j]:
+                        break
+                    dp[j] -= toRemove
+            if prevDelay >= 0:
+                dp[i] += dp[prevDelay] 
+            dp[i] = dp[i] % mod
+        return dp[-1]
+    
+"""
+Ans: 5,6,2
+6
+2
+4
+4
+1
+3
+6
+1
+2
+"""
