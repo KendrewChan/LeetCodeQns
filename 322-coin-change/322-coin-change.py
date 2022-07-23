45fb844cf590cb3230cff2bd327e16ba8cbc005c
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-07-24 00:57:26 +0800

    Time: 2573 ms (30.20%), Space: 14.3 MB (55.54%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -2,18 +2,14 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         # Cant do greedy approach here
         # Lets try dp approach, similar to knapsack problem?
-        overlimit = pow(10,4)+1
-        dp = [overlimit for _ in range(amount+1)]
+        dp = [math.inf for _ in range(amount+1)]
         dp[0] = 0
-        
         for i in range(amount):
-            curr = dp[i]
             for coin in coins:
-                nxt = i+coin
+                nxt = i + coin
                 if nxt > amount:
                     continue
-                dp[nxt] = min(dp[nxt], curr+1)
-        if dp[-1] == overlimit:
+                dp[nxt] = min(dp[nxt], dp[i]+1)
+        if dp[-1] == math.inf:
             return -1
         return dp[-1]
-        
