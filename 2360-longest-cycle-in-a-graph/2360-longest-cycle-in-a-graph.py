7608b737cc82e44c6062f93cb45277a9c3a64410
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-08-01 00:58:48 +0800

    Time: 2298 ms (12.50%), Space: 127.4 MB (37.50%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -1,21 +1,21 @@
 class Solution:    
     def longestCycle(self, edges: List[int]) -> int:
         self.cyclic = -1
-        self.checked = [False]*len(edges)
-        visited = [0]*len(edges)
+        self.checked = [False]*len(edges) # used to prevent repetition
+        visited = [0]*len(edges) # used for calculations
         for node in range(len(edges)):            
             self.dfs(edges, node, 0, visited)
         return self.cyclic
         
     def dfs(self, edges, node, curr, visited):
-        if self.checked[node] or node == -1:
+        if self.checked[node] or node == -1: # If previously checked / end of node
             return
         if visited[node] > 0:
             self.cyclic = max(self.cyclic, curr-visited[node])
             return
         visited[node] = curr
         self.dfs(edges, edges[node], curr+1, visited)
-        self.checked[node] = True
+        self.checked[node] = True # There's a difference in putting this before/after the recursion
         
 """ 4
 [1,2,0,4,5,6,3,8,9,7]
