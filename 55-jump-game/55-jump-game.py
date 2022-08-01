6c630ed95070a967b0b63526e7bec77b1f5135be
Kendrew <34956515+KendrewChan@users.noreply.github.com>
2022-08-02 00:41:38 +0800

    Time: 741 ms (54.37%), Space: 15.2 MB (50.32%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,13 +1,10 @@
-class Solution(object):
-    def canJump(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        n = len(nums)
-        max_avail = 0
-        for i in range(n):
-            if i > max_avail:
-                break
-            max_avail = max(max_avail, i+nums[i])
-        return max_avail >= n-1
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        furthest = 0
+        for i in range(len(nums)):
+            if i > furthest:
+                return False
+            jump = nums[i]
+            furthest = max(furthest, i+jump)
+        return True
+            
